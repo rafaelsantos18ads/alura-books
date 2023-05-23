18118fc183b1e41ce7a4b34c9019b19f88f61fa5
Rafael Santos <rafael.souza@itixti.com.br>
2023-05-22 23:49:45 -0300

    Incluido o evento que retorna os livros

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import Header from './componentes/Header/Index';
 import styled from 'styled-components';
+import Pesquisa from './componentes/Pesquisa/index';
 
 const AppContainer = styled.div`
       
         width: 100vw;
         height: 100vh;
-        background-image: linear-gradient(90deg, #002F52 35%, #326589);
-      
-
-
-      li{
-        list-style: none;
-      }
-`
+        background-image: linear-gradient(90deg, #002F52 35%, #326589); `
 
 function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa/>
       </AppContainer>
     
   );
---
+++ src/componentes/Pesquisa/DadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome:'Liderança em design', id: 1, src:livro},
+    {nome:'Javascript assertivo', id: 2, src:livro},
+    {nome:'Cangaceiro em javascript', id: 3, src:livro},
+    {nome:'Aventureiros do Java', id: 4, src:livro}
+
+]
+
